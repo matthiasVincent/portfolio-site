7182c606dd023625c5ed092a3eebb05d5d00979e
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2024-12-12 15:13:15 +0100

    Make phone link, email link and whatsapp link functional and added latest work experience

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -29,9 +29,9 @@ env.read_env()
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['pythonanywhere.com', 'Matthias28908ue14.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['pythonanywhere.com', 'Matthias28908ue14.pythonanywhere.com', 'localhost', '127.0.0.1', '192.168.0.108']
 
 
 # Application definition
